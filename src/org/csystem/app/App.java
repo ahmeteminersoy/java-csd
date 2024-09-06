d41076d5ffca9e1c449243a72def2d35ce84c570
metamfetemin <ahmet.eminersoy@icloud.com>
2024-09-07 00:00:11 +0300

    first commit

---
+++ src/org/csystem/app/App.java
@@ -0,0 +1,9 @@
+package org.csystem.app;
+
+import org.csystem.app.lottery.NumericLotteryApp;
+
+public class App{
+    public static void main(String[] args) {
+        NumericLotteryApp.run();
+    }
+}
